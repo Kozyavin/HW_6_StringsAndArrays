606bf31223acf18f1657551a5d52e5c19b1c6636
Kozyavin V <kioci@yandex.ru>
2023-04-29 22:44:11 +0300

    ErrorCorrection

--- src/main/java/ru/netology/stats/services/StatsService.java
+++ src/main/java/ru/netology/stats/services/StatsService.java
@@ -27,33 +27,39 @@ public class StatsService {
 
     public int monthOfMaximumSales(int[] arr) {   //номер месяца, в котором был пик продаж, то есть осуществлены продажи на максимальную сумму
         int maxMonth = 0;
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= arr[maxMonth])
                 maxMonth = i;
+        }
         return maxMonth + 1;
     }
 
     public int monthOfMinimumSales(int[] arr) {  //номер месяца, в котором был минимум продаж, то есть осуществлены продажи на минимальную сумму
         int minMonth = 0;
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] <= arr[minMonth])
                 minMonth = i;
+        }
         return minMonth + 1;
     }
 
     public int monthlySalesBelowAverage(int[] arr) {  //количество месяцев, в которых продажи были ниже среднего
         int mba = 0;
-        for (int i = 0; i < arr.length; i++)
-            if (arr[i] < averageMonthlySales(arr)) //сравниваем с средним значением продаж за 12 месяцев
+        int avg = averageMonthlySales(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < avg) //сравниваем с средним значением продаж за 12 месяцев
                 mba = mba + 1;
+        }
         return mba;
     }
 
     public int monthlySalesAboveAverage(int[] arr) { //количество месяцев, в которых продажи были выше среднего
         int mba = 0;
-        for (int i = 0; i < arr.length; i++)
-            if (arr[i] > averageMonthlySales(arr)) //сравниваем с средним значением продаж за 12 месяцев
+        int avg = averageMonthlySales(arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > avg) //сравниваем с средним значением продаж за 12 месяцев
                 mba = mba + 1;
+        }
         return mba;
     }
 }
